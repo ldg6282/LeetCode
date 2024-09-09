aab7019472bec8aa413cb96d17f9ee8736ee1d10
이도건 (Lee Do Geon) <166673478+ldg6282@users.noreply.github.com>
2024-09-09 10:06:54 +0900

    Time: 86 ms (48.17%), Space: 56.5 MB (45.4%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    const result = [];
+  
+    for (let i = 0; i < nums.length; i++) {
+      result.push(nums[i] * nums[i]);
+    }
+  
+    return result.sort((a, b) => a - b);
+};
