8750e3194e2c0bf5c16d2c78d2db7d0b3c6a216c
이도건 (Lee Do Geon) <166673478+ldg6282@users.noreply.github.com>
2024-09-23 08:48:08 +0900

    Time: 62 ms (36.55%), Space: 51.3 MB (34.95%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+    
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 0) {
+            const emptyLeft = (i === 0) || (flowerbed[i - 1] === 0);
+            const emptyRight = (i === flowerbed.length - 1) || (flowerbed[i + 1] === 0);
+            
+            if (emptyLeft && emptyRight) {
+                flowerbed[i] = 1;
+                count++;
+                i++;
+            }
+        }
+    }
+    
+    return count >= n;
+};
